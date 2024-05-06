86215e36c35dabdac57ad0d39463a0e7bcd5a1ff
Jeff DuBois <jeffdubo@gmail.com>
2024-05-06 15:49:59 -0700

    Create blank config for install/setup

---
+++ config_blank.py
@@ -0,0 +1,15 @@
+# Be sure to rename this file to config.py before running app.py
+
+# Set to true to use postgreSQL database, False to use sqlite database
+postgreSQL_flag = True
+
+# Be sure to update all of the variables to connect to your local postgreSQL database
+
+db_username = 'postgres' # Update this if you are not using this default postgreSQL username
+db_password = '[your password]'
+db_host = 'localhost'
+db_port = 5432
+db_name = '[name of your database]'
+
+# API Key for US Census Bureau
+census_key = '[your API key]'
