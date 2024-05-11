e96b8dcd507094bc29d3422c803b60611d45c47f
Jeff DuBois <jeffdubo@gmail.com>
2024-05-10 17:16:12 -0700

    Update file

--- config_blank.py
+++ config_blank.py
@@ -5,11 +5,8 @@ postgreSQL_flag = True
 
 # Be sure to update all of the variables to connect to your local postgreSQL database
 
-db_username = 'postgres' # Update this if you are not using this default postgreSQL username
+db_username = '[username]' # The default in pgAdmin is postgres
 db_password = '[your password]'
 db_host = 'localhost'
 db_port = 5432
 db_name = '[name of your database]'
-
-# API Key for US Census Bureau
-census_key = '[your API key]'
